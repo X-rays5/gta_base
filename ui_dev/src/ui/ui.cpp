2d6754f68b3af2d7880c220bd19cbfcc76cff8ba
X-rays5 <61073708+X-rays5@users.noreply.github.com>
2022-03-09 03:29:27 +0100

    Hello world triangle

--- ui_dev/src/ui/ui.cpp
+++ ui_dev/src/ui/ui.cpp
@@ -8,27 +8,8 @@
 
 namespace ui {
   void Tick() {
-    {
-      static float f = 0.0f;
-      static int counter = 0;
+    auto d3d = ImGui::GetBackgroundDrawList();
 
-      ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
-      ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-
-      ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-
-      if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-        counter++;
-      ImGui::SameLine();
-      ImGui::Text("counter = %d", counter);
-
-      ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-      ImGui::End();
-    }
-
-    ImGui::Begin("Another Window");   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-    ImGui::Text("Hello from another window!");
-    ImGui::End();
+    d3d->AddTriangle(ImVec2(50,50), ImVec2(50, 100), ImVec2(100, 75), ImColor(243, 23, 56));
   }
 }
