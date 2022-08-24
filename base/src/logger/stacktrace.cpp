e30c3bb749fd20513373cf4a2e2d8255417c62dd
X-rays5 <61073708+X-rays5@users.noreply.github.com>
2022-08-24 21:02:02 +0200

    print module crash occured in

--- base/src/logger/stacktrace.cpp
+++ base/src/logger/stacktrace.cpp
@@ -44,6 +44,8 @@ namespace gta_base {
           case EXCEPTION_INVALID_DISPOSITION: return "EXCEPTION_INVALID_DISPOSITION";
           case EXCEPTION_GUARD_PAGE: return "EXCEPTION_GUARD_PAGE";
           case EXCEPTION_INVALID_HANDLE: return "EXCEPTION_INVALID_HANDLE";
+          case DBG_PRINTEXCEPTION_C: return "DBG_PRINTEXCEPTION_C";
+          case CONTROL_C_EXIT: return "CONTROL_C_EXIT";
           default: return "UNKNOWN";
         }
       }
@@ -92,8 +94,8 @@ namespace gta_base {
       }
 
       // Using the given context, fill in all the stack frames.
-      // Which then later can be interpreted to human readable text
-      void captureStackTrace(CONTEXT *context, std::vector<uint64_t> &frame_pointers) {
+      // Which then later can be interpreted to human-readable text
+      void captureStackTrace(CONTEXT* context, std::vector<uint64_t>& frame_pointers) {
         DWORD machine_type = 0;
         STACKFRAME64 frame = {}; // force zeroing
         frame.AddrPC.Mode = AddrModeFlat;
@@ -101,14 +103,14 @@ namespace gta_base {
         frame.AddrStack.Mode = AddrModeFlat;
 #if defined(_M_ARM64)
         frame.AddrPC.Offset = context->Pc;
-      frame.AddrFrame.Offset = context->Fp;
-      frame.AddrStack.Offset = context->Sp;
-      machine_type = IMAGE_FILE_MACHINE_ARM64;
+        frame.AddrFrame.Offset = context->Fp;
+        frame.AddrStack.Offset = context->Sp;
+        machine_type = IMAGE_FILE_MACHINE_ARM64;
 #elif defined(_M_ARM)
         frame.AddrPC.Offset = context->Pc;
-      frame.AddrFrame.Offset = context->R11;
-      frame.AddrStack.Offset = context->Sp;
-      machine_type = IMAGE_FILE_MACHINE_ARM;
+        frame.AddrFrame.Offset = context->R11;
+        frame.AddrStack.Offset = context->Sp;
+        machine_type = IMAGE_FILE_MACHINE_ARM;
 #elif defined(_M_X64)
         frame.AddrPC.Offset = context->Rip;
         frame.AddrFrame.Offset = context->Rbp;
@@ -116,9 +118,9 @@ namespace gta_base {
         machine_type = IMAGE_FILE_MACHINE_AMD64;
 #else
         frame.AddrPC.Offset = context->Eip;
-      frame.AddrPC.Offset = context->Ebp;
-      frame.AddrPC.Offset = context->Esp;
-      machine_type = IMAGE_FILE_MACHINE_I386;
+        frame.AddrPC.Offset = context->Ebp;
+        frame.AddrPC.Offset = context->Esp;
+        machine_type = IMAGE_FILE_MACHINE_I386;
 #endif
         for (size_t index = 0; index < frame_pointers.size(); ++index)
         {
@@ -132,24 +134,58 @@ namespace gta_base {
                           SymGetModuleBase64,
                           NULL)) {
             frame_pointers[index] = frame.AddrPC.Offset;
-          } else {
+          }
+          else {
             break;
           }
         }
       }
 
+      // Gets module name as a std::string from an address
+      void getAllModuleInfos(std::map<std::string, std::pair<uint64_t, uint64_t>>* map) {
+        HMODULE hMods[1024];
+        HANDLE hProcess = GetCurrentProcess();
+        DWORD cbNeeded;
+        if (EnumProcessModules(hProcess, hMods, sizeof(hMods), &cbNeeded)) {
+          for (int i = 0; i < (cbNeeded / sizeof(HMODULE)); i++) {
+            // Get the full path to the module's file.
+            char modNameCharArray[MAX_PATH];
+            if (GetModuleFileNameExA(hProcess, hMods[i], modNameCharArray, sizeof(modNameCharArray) / sizeof(char))) {
+              std::string modName(modNameCharArray);
+              MODULEINFO info;
+              if (GetModuleInformation(hProcess, hMods[i], &info, sizeof(MODULEINFO))) {
+                std::map<uint64_t, uint64_t> modInfo;
+                map->emplace(modName, std::make_pair((uint64_t)info.lpBaseOfDll, (uint64_t)info.SizeOfImage));
+              }
+            }
+          }
+        }
+      }
+
+      // Gets module name and offset from address
+      std::string getModuleNameFromAddress(uint64_t address) {
+        std::map<std::string, std::pair<uint64_t, uint64_t>> map;
+        getAllModuleInfos(&map);
+        for (auto const& [key, val] : map) {
+          if (address > val.first && address < (val.first + val.second)) {
+            std::string retn = key + "+" + AddrToHex(address - val.first);
+            return retn;
+          }
+        }
+        return "";
+      }
 
 
       // extract readable text from a given stack frame. All thanks to
       // using SymFromAddr and SymGetLineFromAddr64 with the stack pointer
-      std::string getSymbolInformation(const size_t index, const std::vector<uint64_t> &frame_pointers) {
+      std::string getSymbolInformation(const size_t index, const std::vector<uint64_t>& frame_pointers) {
         auto addr = frame_pointers[index];
         std::string frame_dump = "stack dump [" + std::to_string(index) + "]\t";
 
         DWORD64 displacement64;
         DWORD displacement;
         char symbol_buffer[sizeof(SYMBOL_INFO) + MAX_SYM_NAME];
-        SYMBOL_INFO *symbol = reinterpret_cast<SYMBOL_INFO *>(symbol_buffer);
+        SYMBOL_INFO* symbol = reinterpret_cast<SYMBOL_INFO*>(symbol_buffer);
         symbol->SizeOfStruct = sizeof(SYMBOL_INFO);
         symbol->MaxNameLen = MAX_SYM_NAME;
 
@@ -160,17 +196,29 @@ namespace gta_base {
         if (SymFromAddr(GetCurrentProcess(), addr, &displacement64, symbol)) {
           callInformation.append(" ").append(std::string(symbol->Name, symbol->NameLen));
           if (SymGetLineFromAddr64(GetCurrentProcess(), addr, &displacement, &line)) {
-            lineInformation.append("\t").append(line.FileName).append(" L: ");
+            lineInformation.append(" ").append(line.FileName).append(" L: ");
             lineInformation.append(std::to_string(line.LineNumber));
           }
+          else {
+            std::string moduleName = getModuleNameFromAddress(addr);
+            if (moduleName != "") {
+              lineInformation.append(" ").append(moduleName);
+            }
+          }
+        }
+        else {
+          std::string moduleName = getModuleNameFromAddress(addr);
+          if (moduleName != "") {
+            lineInformation.append(" ").append(moduleName);
+          }
+          lineInformation.append(" ").append(AddrToHex(addr));
         }
         frame_dump.append(lineInformation).append(callInformation);
         return frame_dump;
       }
 
-
       // Retrieves all the symbols for the stack frames, fills them within a text representation and returns it
-      std::string convertFramesToText(std::vector<uint64_t> &frame_pointers) {
+      std::string convertFramesToText(std::vector<uint64_t>& frame_pointers) {
         std::string dump; // slightly more efficient than ostringstream
         const size_t kSize = frame_pointers.size();
         for (size_t index = 0; index < kSize && frame_pointers[index]; ++index) {
@@ -178,6 +226,32 @@ namespace gta_base {
           dump += "\n\r";
         }
         return dump;
+      }
+
+      std::string GetFileExceptionOccured(PEXCEPTION_RECORD except_rec) {
+        auto location = reinterpret_cast<std::uintptr_t>(except_rec->ExceptionAddress);
+
+        HMODULE Hmods[1024];
+        DWORD cbNeeded;
+        if (EnumProcessModules(GetCurrentProcess(), Hmods, sizeof(Hmods), &cbNeeded)) {
+          for (int i = 0; i < (cbNeeded / sizeof(HMODULE)); i++) {
+            // Get the full path to the module's file.
+            char modNameCharArray[MAX_PATH];
+            if (GetModuleFileNameExA(GetCurrentProcess(), Hmods[i], modNameCharArray, sizeof(modNameCharArray) / sizeof(char))) {
+              std::string modName(modNameCharArray);
+              MODULEINFO info;
+              if (GetModuleInformation(GetCurrentProcess(), Hmods[i], &info, sizeof(MODULEINFO))) {
+                auto min = reinterpret_cast<std::uintptr_t>(info.lpBaseOfDll);
+                auto max = min + info.SizeOfImage;
+                if (location >= min && location < max) {
+                  return modName;
+                }
+              }
+            }
+          }
+        }
+
+        return "UNKNOWN";
       }
 
       std::string GetExceptionString(PEXCEPTION_POINTERS except) {
@@ -191,7 +265,7 @@ namespace gta_base {
 
         std::stringstream msg;
         msg << "\n\r***** EXCEPTION RECEIVED *****\n";
-        msg << "\n\r***** Received fatal exception: " << ExceptionCodeToStr(except_rec->ExceptionCode) << " pid: " << _getpid() << " *****\n";
+        msg << "\n\r***** Received fatal exception: " << ExceptionCodeToStr(except_rec->ExceptionCode) << " pid: " << _getpid() << " module: " << GetFileExceptionOccured(except_rec) << " *****\n";
         msg << "\r***** Exception code: " << AddrToHex(except_rec->ExceptionCode) << " *****\n";
         msg << "\r***** Exception address: 0x" << except_rec->ExceptionAddress << " *****\n";
         msg << "\r***** Exception flags: " << except_rec->ExceptionFlags << " *****\n";
@@ -221,7 +295,15 @@ namespace gta_base {
           const size_t kmax_frame_dump_size = 64;
           std::vector<uint64_t>  frame_pointers(kmax_frame_dump_size);
           captureStackTrace(ctx, frame_pointers);
-          msg << "\n\r" << convertFramesToText(frame_pointers);
+          msg << "\n\r" << convertFramesToText(frame_pointers) << "\n\r";
+
+          msg << "\n\rLoaded Modules:\n\r";
+          std::map<std::string, std::pair<uint64_t, uint64_t>> map;
+          getAllModuleInfos(&map);
+          for (auto const& [key, val] : map) {
+            msg << key + " Base Address: " + AddrToHex(val.first) + " Size: " + AddrToHex(val.second);
+            msg << "\n\r";
+          }
 
           return RemoveDoubleSpaces(msg.str());
         }
