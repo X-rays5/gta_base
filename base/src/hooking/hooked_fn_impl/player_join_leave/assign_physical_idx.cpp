2f29a4b667afaf284b777ccdcc12de0c5d99d7be
X-rays5 <61073708+X-rays5@users.noreply.github.com>
2022-09-09 21:10:28 +0200

    fix leave notifications using a invalid slot

--- base/src/hooking/hooked_fn_impl/player_join_leave/assign_physical_idx.cpp
+++ base/src/hooking/hooked_fn_impl/player_join_leave/assign_physical_idx.cpp
@@ -10,15 +10,17 @@
 
 namespace gta_base {
   void* Hooks::assign_physical_index(CNetworkPlayerMgr* netPlayerMgr, CNetGamePlayer* player, uint8_t new_index) {
+    auto slot = player->m_player_id;
+
     auto res = kHOOKING->assign_physical_index_hook_.GetOriginal<decltype(&Hooks::assign_physical_index)>()(netPlayerMgr, player, new_index);
 
     // leaving
-    if (new_index == 0xFF) {
+    if (new_index == 255) {
       kPLAYER_MGR->RemovePlayer(player);
 
-      auto msg = fmt::format("name: {}\nslot: {}\nRID: {}", player->get_name(), new_index, player->get_net_data()->m_gamer_handle.m_rockstar_id);
+      auto msg = fmt::format("name: {}\nslot: {}\nRID: {}", player->get_name(), slot, player->get_net_data()->m_gamer_handle.m_rockstar_id);
       ui::kNOTIFICATIONS->Create(ui::Notification::Type::kInfo, "Player leaving.", msg);
-      LOG_INFO("{} is leaving. slot: {}. RID: {}", player->get_name(), new_index, player->get_net_data()->m_gamer_handle.m_rockstar_id);
+      LOG_INFO("{} is leaving. slot: {} RID: {}", player->get_name(), slot, player->get_net_data()->m_gamer_handle.m_rockstar_id);
     } else {
       kPLAYER_MGR->AddPlayer(player);
 
