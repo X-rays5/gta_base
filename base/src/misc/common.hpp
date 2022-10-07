276f0da50043aec425a692eabc304140a8cbf13e
X-rays5 <61073708+X-rays5@users.noreply.github.com>
2022-10-07 17:21:32 +0200

    added some functions to the common namespace

--- base/src/misc/common.hpp
+++ base/src/misc/common.hpp
@@ -30,58 +30,63 @@
 #pragma warning(disable:4996)
 
 namespace gta_base::common {
-    enum class Platform {
-      kRockstar,
-      kEpicGames,
-      kSteam
-    };
+  enum class Platform {
+    kRockstar,
+    kEpicGames,
+    kSteam
+  };
 
-    Platform GetCurrentPlatform();
+  Platform GetCurrentPlatform();
 
-    bool IsSessionStarted();
+  bool IsSessionStarted();
 
-    template<typename T>
-    inline T LexicalCast(const std::string& str) {
-      if (str.empty())
-        return {};
+  template<typename T>
+  inline T LexicalCast(const std::string& str) {
+    if (str.empty())
+      return {};
 
-      T res;
-      std::istringstream iss;
-      iss.str(str);
-      iss >> res;
-      return res;
-    }
-
-    template<typename in_t, typename ret_t>
-    inline ret_t WithinLimits(in_t convert) {
-      constexpr static const in_t min = std::numeric_limits<ret_t>::min();
-      constexpr static const in_t max = std::numeric_limits<ret_t>::max();
-
-      return static_cast<ret_t>(std::clamp(convert, min, max));
-    }
-
-    std::string FloatPrecision(float num, std::size_t precision = 3);
-
-    std::filesystem::path GetBaseDir();
-    std::filesystem::path GetConfigDir();
-    std::filesystem::path GetLogDir();
-    std::filesystem::path GetLogFile();
-    std::filesystem::path GetLogSaveDir();
-    std::filesystem::path GetDataDir();
-    std::filesystem::path GetSettingsDir();
-    std::filesystem::path GetTeleportsDir();
-    std::filesystem::path GetTextureDir();
-
-    std::uint64_t GetEpoch();
-
-    HWND GetHwnd(const char* class_name, const char* window_name);
-    HWND GetGameHwnd();
-    bool IsForegroundWindow();
-    bool IsTargetProcess();
-
-    bool IsKeyDown(std::uint32_t key);
-    bool IsKeyJustReleased(std::uint32_t key, std::uint64_t since_up = 100);
-    void SetKeyState(std::uint32_t key, bool down);
+    T res;
+    std::istringstream iss;
+    iss.str(str);
+    iss >> res;
+    return res;
   }
+
+  template<typename in_t, typename ret_t>
+  inline ret_t WithinLimits(in_t convert) {
+    constexpr static const in_t min = std::numeric_limits<ret_t>::min();
+    constexpr static const in_t max = std::numeric_limits<ret_t>::max();
+
+    return static_cast<ret_t>(std::clamp(convert, min, max));
+  }
+
+  std::string VkToStr(std::uint64_t vk);
+
+  std::string FloatPrecision(float num, std::size_t precision = 3);
+
+  std::filesystem::path GetBaseDir();
+  std::filesystem::path GetConfigDir();
+  std::filesystem::path GetLogDir();
+  std::filesystem::path GetLogFile();
+  std::filesystem::path GetLogSaveDir();
+  std::filesystem::path GetDataDir();
+  std::filesystem::path GetSettingsDir();
+  std::filesystem::path GetThemesDir();
+  std::filesystem::path GetHotkeysDir();
+  std::filesystem::path GetOptionsStateDir();
+  std::filesystem::path GetTeleportsDir();
+  std::filesystem::path GetTextureDir();
+
+  std::uint64_t GetEpoch();
+
+  HWND GetHwnd(const char* class_name, const char* window_name);
+  HWND GetGameHwnd();
+  bool IsForegroundWindow();
+  bool IsTargetProcess();
+
+  bool IsKeyDown(std::uint32_t key);
+  bool IsKeyJustReleased(std::uint32_t key, std::uint64_t since_up = 100);
+  void SetKeyState(std::uint32_t key, bool down);
+}
 
 #endif //GTABASE_COMMON_HPP
