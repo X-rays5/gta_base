8b9490f4157aa7387c894392b3a796f010fc8719
X-rays5 <61073708+X-rays5@users.noreply.github.com>
2022-11-12 15:51:15 +0100

    added pch to improve compile time

---
+++ base/src/pch.hpp
@@ -0,0 +1,13 @@
+//
+// Created by X-ray on 11/12/22.
+//
+
+#pragma once
+#ifndef GTA_BASE_PCH_HPP
+#define GTA_BASE_PCH_HPP
+#include "logger/logger.hpp"
+#include "misc/common.hpp"
+#include "natives/natives.hpp"
+#include "misc/globals.hpp"
+#include "misc/settings.hpp"
+#endif //GTA_BASE_PCH_HPP
