eddfdce190e613aa2a88717513e12ddf91474c31
X-rays5 <61073708+X-rays5@users.noreply.github.com>
2022-12-12 03:00:02 +0100

    remove random copilot comments in example lua

--- base/example-scripts/test.lua
+++ base/example-scripts/test.lua
@@ -1,8 +1,5 @@
 config = {
 	gameroot = "C:/Users/username/Documents/My Games/My Game",
-	-- The path to the game's root directory
-	-- This is where the game will look for the game's data files
-	-- This is also where the game will save its data files
 }
 
 function T()
