302c2fdf2ab8d3bde28eb62457f0a9a3a5425d9a
X-rays5 <61073708+X-rays5@users.noreply.github.com>
2022-12-15 02:07:18 +0100

    update freetype

--- vendor/freetype.cmake
+++ vendor/freetype.cmake
@@ -1,4 +1,4 @@
-CPMAddPackage("gh:freetype/freetype#c943d408e08fbe6d3e12f647b4a10d94a8198e9b")
+CPMAddPackage("gh:freetype/freetype#ace97a02a4461bbdae29da4019c105eead95e277")
 
 target_include_directories(${PROJECT_NAME} PRIVATE ${freetype_SOURCE_DIR}/include)
 target_link_libraries(${PROJECT_NAME} PRIVATE freetype)
