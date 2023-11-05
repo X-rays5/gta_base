b362ea5d51846ed83483c3fbad5522a9074838aa
X-rays5 <61073708+X-rays5@users.noreply.github.com>
2023-11-05 13:03:35 +0100

    added spinlock from v1.0

---
+++ menu/src/util/spinlock.hpp
@@ -0,0 +1,49 @@
+//
+// Created by X-ray on 02/10/23.
+//
+
+#pragma once
+#ifndef GTA_BASE_SPINLOCK_82A7FE9438D94F2093F7FC89AF84E32E_HPP
+#define GTA_BASE_SPINLOCK_82A7FE9438D94F2093F7FC89AF84E32E_HPP
+#include <atomic>
+#include <thread>
+
+namespace base::util {
+  class Spinlock {
+  public:
+    void Lock() noexcept;
+    bool TryLock() noexcept;
+    void Unlock() noexcept;
+
+  private:
+    std::atomic<bool> lock_ = false;
+  };
+
+  class RecursiveSpinlock {
+  public:
+    void Lock() noexcept;
+    bool TryLock() noexcept;
+    void Unlock() noexcept;
+
+  private:
+    Spinlock spinlock_;
+    std::atomic<std::thread::id> cur_locking_thread_;
+    std::thread::id default_thread_id_;
+    std::size_t lock_count_{};
+  };
+
+  template<class T> requires std::is_same_v<T, Spinlock> or std::is_same_v<T, RecursiveSpinlock>
+  class ScopedSpinlock {
+  public:
+    explicit ScopedSpinlock(T& lock) noexcept: lock_(lock) {
+      lock_.Lock();
+    }
+    ~ScopedSpinlock() noexcept {
+      lock_.Unlock();
+    }
+
+  private:
+    T& lock_;
+  };
+}
+#endif //GTA_BASE_SPINLOCK_82A7FE9438D94F2093F7FC89AF84E32E_HPP
