e8e515958e989f7c5c0fb1b98dcd39690cca45f0
X-rays5 <61073708+X-rays5@users.noreply.github.com>
2024-04-13 00:32:05 +0200

    Fix decompile non executable address

--- vendor/zydis.cmake
+++ vendor/zydis.cmake
@@ -1,6 +1,6 @@
 option(ZYDIS_BUILD_TOOLS "" OFF)
 option(ZYDIS_BUILD_EXAMPLES "" OFF)
 option(ZYDIS_BUILD_DOXYGEN "" OFF)
-cpmaddpackage("gh:zyantific/zydis#ba1e3cf3a64f668790164c005d1ee52f3d77cf76")
+cpmaddpackage("gh:zyantific/zydis#15e38ac")
 
 target_link_libraries(${PROJECT_NAME} PRIVATE Zydis)
