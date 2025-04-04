044e9271df50a7c2dc8d5ca436a814814d7323a3
X-rays5 <61073708+X-rays5@users.noreply.github.com>
2025-04-04 12:33:28 +0200

    Update SDL 3.2.4 -> 3.2.10

--- vendor/sdl.cmake
+++ vendor/sdl.cmake
@@ -8,6 +8,6 @@ set(SDL_POWER OFF CACHE BOOL "Disable Power subsystem in SDL3" FORCE)
 set(SDL_SENSOR OFF CACHE BOOL "Disable Sensor subsystem in SDL3" FORCE)
 set(SDL_DIALOG OFF CACHE BOOL "Disable Dialog subsystem in SDL3" FORCE)
 
-CPMAddPackage("gh:libsdl-org/SDL#release-3.2.4")
+CPMAddPackage("gh:libsdl-org/SDL#release-3.2.10")
 
 target_link_libraries(${PROJECT_NAME} PUBLIC SDL3::SDL3)
