2d2b06d0064a810c1c3c6ba8abfb08257745e0e5
X-rays5 <61073708+X-rays5@users.noreply.github.com>
2025-04-04 12:57:20 +0200

    Disable lunasvg with imgui

--- vendor/imgui.cmake
+++ vendor/imgui.cmake
@@ -1,6 +1,7 @@
 set(ENABLE_DX11_BACKEND ON)
 set(ENABLE_SDL3_BACKEND ON)
 set(IMGUI_DISABLE_OBSOLETE_FUNCTIONS OFF) # Without this off the dx11 renderer backend is broken.
+set(IMGUI_ENABLE_FREETYPE_LUNASVG OFF)
 
 cpmaddpackage("gh:X-rays5/imgui_cmake#master")
 
