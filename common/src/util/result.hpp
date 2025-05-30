ead190f425c906d127cc2ecbd0145f971f7708ce
X-rays5 <61073708+X-rays5@users.noreply.github.com>
2025-05-30 22:12:29 +0200

    Remove make success from result

--- common/src/util/result.hpp
+++ common/src/util/result.hpp
@@ -101,14 +101,6 @@ namespace base::common::util::result {
     }
     return cpp::fail(StatusErr(code, std::move(message)));
   }
-
-  /**
-   * Make a success status
-   * @return a cpp::failure instance containing kSUCCESS
-   */
-  [[nodiscard]] inline cpp::failure<StatusErr> MakeSuccess() {
-    return MakeFailure<ResultCode::kSUCCESS>();
-  }
 }
 
 namespace base {
