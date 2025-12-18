839ad340b0fb80e25917fd2997ae31a2efe5d4da
X-ray <61073708+X-rays5@users.noreply.github.com>
2025-12-18 22:05:17 +0100

    Add unit tests projects to projects missing them (#81)
    * Drop atl dep
    
    * Add unit test base to injector and menu

--- injector/vendor.cmake
+++ injector/vendor.cmake
@@ -1,5 +1,10 @@
+set(PROJECT_NAME_COPY ${PROJECT_NAME})
+set(PROJECT_NAME ${PROJECT_NAME}_core)
+
 include(../vendor/sdl.cmake)
 include(../vendor/glaze.cmake)
 include(../vendor/magic_enum.cmake)
 include(../vendor/imgui.cmake)
 target_link_libraries(imgui PRIVATE SDL3::SDL3)
+
+set(PROJECT_NAME ${PROJECT_NAME_COPY})
