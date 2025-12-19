731e1a161e3690a61a4cdfc97d970a0f32bd4489
X-rays5 <61073708+X-rays5@users.noreply.github.com>
2025-12-19 10:23:58 +0100

    Update discord-rpc

--- vendor/discord-rpc.cmake
+++ vendor/discord-rpc.cmake
@@ -1,4 +1,4 @@
-cpmaddpackage("gh:X-rays5-forks/discord-rpc#7b5ef33efc78b593c17ed371566fb5def882b312")
+cpmaddpackage("gh:X-rays5-forks/discord-rpc#6bd5ea640fe669e0912e047d0b8c2b846ea81ed0")
 
 target_include_directories(${PROJECT_NAME} PUBLIC ${discord-rpc_SOURCE_DIR}/include)
 target_link_libraries(${PROJECT_NAME} PUBLIC discord-rpc)
