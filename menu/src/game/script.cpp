9d52c141d11bc5a47e9fae0afd86d625eea62614
X-rays5 <61073708+X-rays5@users.noreply.github.com>
2026-03-22 14:38:43 +0100

    Use RAII for TLS change RunAsScript

--- menu/src/game/script.cpp
+++ menu/src/game/script.cpp
@@ -7,6 +7,36 @@
 #include "../memory/pointers.hpp"
 
 namespace base::menu::game {
+  namespace {
+    class ScriptTlsGuard {
+    public:
+      explicit ScriptTlsGuard(rage::script::Thread* new_thread) {
+        tls_ = rage::TlsContext::Get();
+        old_thread_ = tls_->current_script_thread;
+        old_running_in_scrthread_ = tls_->script_thread_active;
+
+        tls_->current_script_thread = new_thread;
+        tls_->script_thread_active = true;
+      }
+
+      ~ScriptTlsGuard() {
+        if (!tls_)
+          return;
+
+        tls_->script_thread_active = old_running_in_scrthread_;
+        tls_->current_script_thread = old_thread_;
+      }
+
+      ScriptTlsGuard(const ScriptTlsGuard&) = delete;
+      ScriptTlsGuard& operator=(const ScriptTlsGuard&) = delete;
+
+    private:
+      rage::TlsContext* tls_{};
+      rage::script::Thread* old_thread_{};
+      bool old_running_in_scrthread_{};
+    };
+  }
+
   rage::script::Thread* FindScriptThread(common::conversion::joaat_t hash) {
     for (const auto& thread : *memory::kPOINTERS->scriptThreads_)
     {
@@ -20,12 +50,10 @@ namespace base::menu::game {
   }
 
   void RunAsScript(rage::script::Thread* thread, const std::function<void()>& callback) {
-    const auto og_thread = rage::TlsContext::Get()->current_script_thread;
-    const auto og_running_in_scrthread = rage::TlsContext::Get()->script_thread_active;
-    rage::TlsContext::Get()->current_script_thread = thread;
-    rage::TlsContext::Get()->script_thread_active = true;
-    callback();
-    rage::TlsContext::Get()->script_thread_active = og_running_in_scrthread;
-    rage::TlsContext::Get()->current_script_thread = og_thread;
+    if (!thread)
+      return;
+
+    ScriptTlsGuard tls_guard(thread);
+      callback();
   }
 }
