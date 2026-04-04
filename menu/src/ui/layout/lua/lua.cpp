668358133865e0a0c004416fd1d802d7bdef6f5b
X-rays5 <61073708+X-rays5@users.noreply.github.com>
2026-04-04 20:35:14 +0200

    Display script info in UI

--- menu/src/ui/layout/lua/lua.cpp
+++ menu/src/ui/layout/lua/lua.cpp
@@ -33,14 +33,19 @@ namespace base::menu::ui::layout {
       }
     }
 
-    std::vector<std::filesystem::path> GetLuaScripts() {
-      common::concurrency::ScopedSpinlock lock(lua_scripts_lock);
-      return lua_scripts;
-    }
-
     std::vector<lua::ScriptMetaData> GetValidScripts() {
       common::concurrency::ScopedSpinlock lock(lua_scripts_lock);
       return valid_scripts;
+    }
+
+    void SetCurrentLuaScript(const lua::ScriptMetaData& meta_data) {
+      common::concurrency::ScopedSpinlock lock(lua_scripts_lock);
+      current_lua_script = meta_data;
+    }
+
+    lua::ScriptMetaData GetCurrentLuaScript() {
+      common::concurrency::ScopedSpinlock lock(lua_scripts_lock);
+      return current_lua_script;
     }
   }
 
@@ -52,6 +57,32 @@ namespace base::menu::ui::layout {
         script.Start().wait();
         LOG_INFO("Finished run");
       }));
+      sub->AddComponent(components::LabelComponent("label/script_info"));
+      const auto curr = GetCurrentLuaScript();
+      sub->AddComponent(components::ExecuteComponent("label/script_name", curr.GetName(), []{}));
+      sub->AddComponent(components::ExecuteComponent("label/script_main_file", curr.GetMainFile().filename().string(), []{}));
+      sub->AddComponent(components::ExecuteComponent("label/script_desc", curr.GetDescription().value_or("label/script_no_desc"), []{}));
+
+      const auto authors = curr.GetAuthor();
+      if (!authors.empty()) {
+        std::string authors_str = authors[0];
+        for (size_t i = 1; i < authors.size(); ++i) {
+          authors_str += ", " + authors[i];
+        }
+        sub->AddComponent(components::ExecuteComponent("label/script_authors", authors_str, []{}));
+      }
+
+      if (const auto version = curr.GetVersion()) {
+        sub->AddComponent(components::ExecuteComponent("label/script_version", version.value(), []{}));
+      }
+
+      if (const auto repo = curr.GetRepository()) {
+        sub->AddComponent(components::ExecuteComponent("label/script_repository", repo.value(), []{}));
+      }
+
+      if (const auto license = curr.GetLicense()) {
+        sub->AddComponent(components::ExecuteComponent("label/script_license", license.value().filename().string(), []{}));
+      }
     });
     kMENU_RENDERER->AddSubmenu(SubmenuIDs::kLUA_SCRIPT, std::move(selected_sub));
   }
