e3fe66c31cd189f490c20b385fa0833178c3a084
X-rays5 <61073708+X-rays5@users.noreply.github.com>
2026-09-09 23:51:40 +0200

    Switch to discord-presence fork

--- vendor/discord-presence.cmake
+++ vendor/discord-presence.cmake
@@ -1,3 +1,3 @@
-CPMAddPackage("gh:EclipseMenu/discord-presence#3b50b0854ec24f9b2d4cc8e28f9145f276bb998e")
+CPMAddPackage("gh:X-rays5-forks/discord-presence#c568a61cc05baac4e45f0392442cd932f901f19e")
 
 target_link_libraries(${PROJECT_NAME} PUBLIC discord-rpc)
