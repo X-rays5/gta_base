eb5a0f06d947bb72780b86cada64538000c54b23
X-rays5 <61073708+X-rays5@users.noreply.github.com>
2026-09-11 01:09:31 +0200

    Update unordered dense

--- vendor/unordered_dense.cmake
+++ vendor/unordered_dense.cmake
@@ -1,3 +1,3 @@
-cpmaddpackage("gh:martinus/unordered_dense@4.8.1")
+cpmaddpackage("gh:martinus/unordered_dense@4.11.0")
 
 target_link_libraries(${PROJECT_NAME} PUBLIC unordered_dense::unordered_dense)
